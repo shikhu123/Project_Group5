2bf54d3a0756159d19cb52c2055a90cf2ec5c504
shikhu123 <shikhap557@gmail.com>
2021-04-07 01:51:42 -0400

    modification in weather

--- js/weather.js
+++ js/weather.js
@@ -1,69 +1,76 @@
 let weather = {
-  apiKey: "4e8f8df6a8188cf75777403b5405bbc1",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    const { sunset } = data.sunset;
-    const { sunrise } = data.sunrise;
-    const { pressure } = data.pressure;
-    const { visibility } = data.visibility;
-    const { feelslike } = data.feelslike;
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".sunset").innerText =
-      "Sunset: " + speed + " km/h";
-    document.querySelector(".sunrise").innerText =
-      "Sunrise: " + speed + " km/h"; 
-    document.querySelector(".pressure").innerText =
-      "Pressure: " + speed + " km/h"; 
-    document.querySelector(".visibility").innerText =
-      "Visibility: " + speed + " km/h"; 
-    document.querySelector(".feelslike").innerText =
-      "Feels Like: " + speed + " km/h";    
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
+	apiKey: "4e8f8df6a8188cf75777403b5405bbc1",
+	fetchWeather: function (city) {
+		fetch(
+				"https://api.openweathermap.org/data/2.5/weather?q=" +
+				city +
+				"&units=metric&appid=" +
+				this.apiKey
+			)
+			.then((response) => {
+				if (!response.ok) {
+					alert("No weather found.");
+					throw new Error("No weather found.");
+				}
+				return response.json();
+			})
+			.then((data) => this.displayWeather(data));
+	},
+	displayWeather: function (data) {
+		const {
+			name
+		} = data;
+		const {
+			icon,
+			description
+		} = data.weather[0];
+		const {
+			temp,
+			humidity
+		} = data.main;
+		const {
+			speed
+		} = data.wind;
+		const {
+			sunset
+		} = data.sunset;
+		const {
+			sunrise
+		} = data.sunrise;
+
+		document.querySelector(".city").innerText = "Weather in " + name;
+		document.querySelector(".icon").src =
+			"https://openweathermap.org/img/wn/" + icon + ".png";
+		document.querySelector(".description").innerText = description;
+		document.querySelector(".temp").innerText = temp + "°C";
+		documdocument.querySelector(".sunrise").innerText =
+			"Sunrise: " + sunrise + " AM";
+		document.querySelector(".sunset").innerText =
+			"Sunset: " + sunset + " PM";
+		document.querySelector(".humidity").innerText =
+			"Humidity: " + humidity + "%";
+		document.querySelector(".wind").innerText =
+			"Wind speed: " + speed + " km/h";
+
+		document.querySelector(".weather").classList.remove("loading");
+		document.body.style.backgroundImage =
+			"url('https://source.unsplash.com/1600x900/?" + name + "')";
+	},
+	search: function () {
+		this.fetchWeather(document.querySelector(".search-bar").value);
+	},
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
+	weather.search();
 });
 
 document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+	.querySelector(".search-bar")
+	.addEventListener("keyup", function (event) {
+		if (event.key == "Enter") {
+			weather.search();
+		}
+	});
 
 weather.fetchWeather("Scarborough");
