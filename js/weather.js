42e48bce7a7e5fbdebf7fd890592fa3510ba1d60
shikhu123 <shikhap557@gmail.com>
2021-04-07 01:16:53 -0400

    modification in weather

--- js/weather.js
+++ js/weather.js
@@ -24,13 +24,16 @@ let weather = {
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
+    documdocument.querySelector(".sunrise").innerText =
+      "Sunrise: " + speed + " AM";
+document.querySelector(".pressure").innerText =
+      "Sunset: " + speed + " PM";ent.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
       "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
+document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
